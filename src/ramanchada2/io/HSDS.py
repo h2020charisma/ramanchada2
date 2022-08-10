657aad3ace49a21f2f992d47f5877eebfacb0ecd
vedina <jeliazkova.nina@gmail.com>
2022-08-10 13:01:28 +0300

    update to work with remote hsds service

--- src/ramanchada2/io/HSDS.py
+++ src/ramanchada2/io/HSDS.py
@@ -2,7 +2,7 @@
 
 from typing import Tuple, Dict
 
-import h5py
+import h5py, h5pyd
 import logging
 import numpy as np
 import numpy.typing as npt
@@ -16,10 +16,11 @@ logger = logging.getLogger()
 @pydantic.validate_arguments(config=dict(arbitrary_types_allowed=True))
 def write_cha(filename: str,
               dataset: str,
-              x: npt.NDArray, y: npt.NDArray, meta: Dict):
+              x: npt.NDArray, y: npt.NDArray, meta: Dict, h5module = None):
     data = np.stack([x, y])
     try:
-        with h5py.File(filename, 'a') as h5:
+        _h5 = h5module or h5py
+        with _h5.File(filename, mode= 'a') as h5:
             if h5.get(dataset) is None:
                 ds = h5.create_dataset(dataset, data=data)
                 ds.attrs.update(meta)
@@ -30,9 +31,10 @@ def write_cha(filename: str,
 
 
 def read_cha(filename: str,
-             dataset: str
+             dataset: str, h5module = None
              ) -> Tuple[npt.NDArray, npt.NDArray, Dict]:
-    with h5py.File(filename, 'r') as h5:
+    _h5 = h5module or h5py
+    with _h5.File(filename, mode= 'r') as h5:
         data = h5.get(dataset)
         if data is None:
             raise ChadaReadNotFoundError(f'dataset `{dataset}` not found in file `{filename}`')
--- src/ramanchada2/spectrum/creators/from_chada.py
+++ src/ramanchada2/spectrum/creators/from_chada.py
@@ -6,9 +6,10 @@ from ramanchada2.io.HSDS import read_cha
 from ramanchada2.misc.spectrum_deco import add_spectrum_constructor
 from ..spectrum import Spectrum
 
+import h5py
 
 @add_spectrum_constructor(set_applied_processing=False)
 @pydantic.validate_arguments
-def from_chada(filename: str, dataset: str = '/raw'):
-    x, y, meta = read_cha(filename, dataset)
+def from_chada(filename: str, dataset: str = '/raw', h5module=None):
+    x, y, meta = read_cha(filename, dataset, h5module=h5module)
     return Spectrum(x=x, y=y, metadata=meta)  # type: ignore
