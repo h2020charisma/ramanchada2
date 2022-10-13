f873bf34d9e134f48ebed51fcddf2c83012378db
Georgi Georgiev <georgiev.georgi.st@gmail.com>
2022-10-13 14:07:25 +0300

    modify unity normalization and add unity_continuous

--- src/ramanchada2/spectrum/calibration/normalize.py
+++ src/ramanchada2/spectrum/calibration/normalize.py
@@ -12,10 +12,19 @@ from ..spectrum import Spectrum
 @validate_arguments(config=dict(arbitrary_types_allowed=True))
 def normalize(old_spe: Spectrum,
               new_spe: Spectrum, /,
-              strategy: Literal['minmax', 'unity'] = 'minmax'):
+              strategy: Literal['unity', 'min_unity', 'unity_continuous', 'minmax'] = 'minmax'):
     if strategy == 'unity':
+        res = old_spe.y
+        res /= np.sum(res)
+        new_spe.y = res
+    elif strategy == 'min_unity':
         res = old_spe.y - np.min(old_spe.y)
         res /= np.sum(res)
+        new_spe.y = res
+    if strategy == 'unity_continuous':
+        res = old_spe.y
+        res /= np.sum(res)
+        res /= np.diff(old_spe.x_bin_boundaries)
         new_spe.y = res
     elif strategy == 'minmax':
         res = old_spe.y - np.min(old_spe.y)
