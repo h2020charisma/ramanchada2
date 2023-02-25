4001a7d021de303a339a155a69b0799040f8da0e
Dirk Lellinger <dlellinger@users.sourceforge.net>
2023-02-25 22:05:39 +0100

    Add PearsonIVAmplitudeParametrizationHPW and tests

---
+++ src/ramanchada2/fitting_functions/pearsonivamplitudeparametrizationhpw.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from ramanchada2.fitting_functions.voigtareaparametrizationnu import (
+    PositionAreaHeightFWHM,
+)
+from scipy.special import betaln, gammaln, loggamma
+
+Log2 = 0.69314718055994530941723212145818  # Math.Log(2)
+
+
+def SafeSqrt(x):
+    if x > 0:
+        return np.sqrt(x)
+    else:
+        return 0.0
+
+
+class PearsonIVAmplitudeParametrizationHPW:
+
+    numberOfTerms = 1
+    orderOfBaselinePolynomial = -1
+
+    def __init__(self, numberOfTerms, orderOfBackgroundPolynomial):
+        self.numberOfTerms = numberOfTerms
+        self.orderOfBaselinePolynomial = orderOfBackgroundPolynomial
+
+    def GetYOfOneTerm(x, height, pos, w, m, v):
+        arg = np.sqrt((np.power(2, 1 / m) - 1) * (1 + v * v)) * (x - pos) / w - v
+        return height * np.exp(
+            m
+            * (
+                np.log((1 + v * v) / (1 + arg * arg))
+                - 2 * v * (np.arctan(arg) + np.arctan(v))
+            )
+        )
+
+    def func(self, pars, x, data=None):
+        sum = np.zeros(len(x))
+        for i in range(self.orderOfBaselinePolynomial, -1, -1):
+            sum *= x
+            sum += pars[f"b{i}"]
+
+        for i in range(self.numberOfTerms):
+            a, xc, w, m, v = (
+                pars[f"a{i}"],
+                pars[f"pos{i}"],
+                pars[f"w{i}"],
+                pars[f"m{i}"],
+                pars[f"v{i}"],
+            )
+            arg = np.sqrt((np.power(2, 1 / m) - 1) * (1 + v * v)) * (x - xc) / w - v
+            sum += a * np.exp(
+                m
+                * (
+                    np.log((1 + v * v) / (1 + arg * arg))
+                    - 2 * v * (np.arctan(arg) + np.arctan(v))
+                )
+            )
+
+        if data is None:
+            return sum
+        return sum - data
+
+    def dfunc(self, pars, x, data=None):
+        result = []
+        k = 0
+
+        # first the derivatives of the peak terms
+        for i in range(self.numberOfTerms):
+            height, pos, w, m, v = (
+                pars[f"a{i}"],
+                pars[f"pos{i}"],
+                pars[f"w{i}"],
+                pars[f"m{i}"],
+                pars[f"v{i}"],
+            )
+            twoToOneByM_1 = np.power(2, 1 / m) - 1
+            ww = w / np.sqrt(twoToOneByM_1 * (1 + v * v))
+            z = (x - pos) / ww - v
+            log1v2_1z2 = np.log((1 + v * v) / (1 + z * z))
+            atanZ_p_V = 2 * v * (np.arctan(z) + np.arctan(v))
+            body = np.exp(m * (log1v2_1z2 - atanZ_p_V))
+            dbodydz = -body * (2 * m * (z + v)) / (1 + z * z)
+
+            dfdheight = np.full(len(x), np.NaN)
+            dfdpos = np.full(len(x), np.NaN)
+            dfdw = np.full(len(x), np.NaN)
+            dfdm = np.full(len(x), np.NaN)
+            dfdv = np.full(len(x), np.NaN)
+
+            dfdheight = body
+            dfdpos = -height * dbodydz / ww
+            dfdw = -height * dbodydz * (z + v) / w
+            dfdm = (
+                height
+                * body
+                * (
+                    -atanZ_p_V
+                    + log1v2_1z2
+                    + Log2
+                    * np.square(z + v)
+                    * np.power(2, 1 / m)
+                    / (twoToOneByM_1 * (1 + z * z) * m)
+                )
+            )
+            dfdv = (
+                -height
+                * 2
+                * m
+                * body
+                * (
+                    (z + v) * (z * v - 1) / ((1 + z * z) * (1 + v * v))
+                    + np.arctan(z)
+                    + np.arctan(v)
+                )
+            )
+
+            result.append(dfdheight)
+            result.append(dfdpos)
+            result.append(dfdw)
+            result.append(dfdm)
+            result.append(dfdv)
+            k += 5
+
+        # second the baseline derivatives
+        if self.orderOfBaselinePolynomial >= 0:
+            xn = np.ones(len(x))
+            for i in range(self.orderOfBaselinePolynomial + 1):
+                result.append(np.copy(xn))
+                k += 1
+                xn *= x
+        return np.array(result)
+
+    def GetInitialParametersFromHeightPositionAndWidthAtRelativeHeight(
+        height, position, fullWidth, relativeHeight=0.5
+    ):
+        if not (relativeHeight > 0 and relativeHeight < 1):
+            raise ValueError("RelativeHeight should be in the open interval (0, 1)")
+
+        # we evaluate the parameters here for a pure Lorentzian (m=1, v=0)
+        w = np.abs(0.5 * fullWidth * np.sqrt(relativeHeight / (1 - relativeHeight)))
+        return [height, position, w, 1, 0]  # Parameters for the Lorentz limit
+
+    def AddInitialParametersFromHeightPositionAndWidthAtRelativeHeight(
+        paras, indexOfPeak, height, position, fullWidth, relativeHeight=0.5
+    ):
+        apwmv = PearsonIVAmplitudeParametrizationHPW.GetInitialParametersFromHeightPositionAndWidthAtRelativeHeight(
+            height, position, fullWidth, relativeHeight
+        )
+        paras.add(f"area{indexOfPeak}", apwmv[0])
+        paras.add(f"pos{indexOfPeak}", apwmv[1])
+        paras.add(f"w{indexOfPeak}", apwmv[2])
+        paras.add(f"m{indexOfPeak}", apwmv[3])
+        paras.add(f"v{indexOfPeak}", apwmv[3])
+
+    def GetFWHMApproximation(w, m, v):
+        sq = np.sqrt((np.power(2, 1 / m) - 1) * (1 + v * v))
+        ws = w / sq
+        ms = m
+        vs = 2 * m * v
+        return (
+            ws
+            * np.sqrt(np.power(2, 1 / ms) - 1)
+            * (np.pi / np.arctan2(np.exp(1) * ms, np.abs(vs)))
+        )
+
+    def GetFWHM(w, m, v):
+        return PearsonIVAmplitudeParametrizationHPW.GetHWHM(
+            w, m, v, True
+        ) + PearsonIVAmplitudeParametrizationHPW.GetHWHM(w, m, v, False)
+
+    def GetHWHM(w, m, v, rightSide):
+        sq = np.sqrt((np.power(2, 1 / m) - 1) * (1 + v * v))
+        return PearsonIVAmplitudeParametrizationHPW.GetHWHMOfPearson4Original(
+            w / sq, m, 2 * m * v, rightSide
+        )
+
+    def GetHWHMOfPearson4Original(w, m, v, rightSide):
+        if not m > 0:
+            return np.NaN
+
+        w = np.abs(w)
+        sign = 1 if rightSide else -1
+        z0 = -v / (2 * m)
+
+        def funcsimp(z, m, v):
+            return (
+                np.log(2)
+                + v * (np.arctan(z0) - np.arctan(z))
+                + m
+                * (
+                    np.log(1 + z0 * z0)
+                    - (
+                        2 * np.log(np.abs(z))
+                        if np.abs(z) > 1e100
+                        else np.log(1 + z * z)
+                    )
+                )
+            )
+
+        def dervsimp(z, m, v):
+            return (
+                (-v - 2 * m * z) / (1 + z * z)
+                if np.abs(z) < 1e100
+                else (-v / z - 2 * m) / z
+            )
+
+        # go forward in exponentially increasing steps, until the amplitude falls below ymaxHalf, in order to bracked the solution
+        zNear = z0
+        zFar = z0
+        d = 1.0
+
+        while np.isfinite(d):
+            zFar = z0 + d * sign
+            y = funcsimp(zFar, m, v)
+            if y < 0:
+                break
+            else:
+                zNear = zFar
+            d *= 2
+        if zNear > zFar:
+            (zNear, zFar) = (zFar, zNear)
+
+        # use Newton-Raphson to refine the result
+        z = 0.5 * (zNear + zFar)  # starting value
+        for i in range(40):
+            funcVal = funcsimp(z, m, v)
+            if rightSide:
+                if funcVal > 0 and z > zNear:
+                    zNear = z
+                if funcVal < 0 and z < zFar:
+                    zFar = z
+            else:  # leftSide
+                if funcVal < 0 and z > zNear:
+                    zNear = z
+                if funcVal > 0 and z < zFar:
+                    zFar = z
+
+            dz = funcVal / dervsimp(z, m, v)
+            znext = z - dz
+            if znext <= zNear:
+                znext = (z + zNear) / 2
+            elif znext >= zFar:
+                znext = (z + zFar) / 2
+
+            if z == znext:
+                break
+
+            z = znext
+
+            if np.abs(dz) < 5e-15 * np.abs(z):
+                break
+        return np.abs((z - z0) * w)
+
+    def GetArea(amp, w, m, v):
+        lnprefactor = (
+            betaln(m - 0.5, 0.5)
+            + 2 * gammaln(m)
+            - 2 * np.real(loggamma(m + m * v * 1.0j))
+        )
+        tmm1 = np.power(2, 1 / m) - 1
+        lnbody = (
+            -2 * m * v * np.arctan(v)
+            + (m - 0.5) * np.log(1 + v * v)
+            - 0.5 * np.log(tmm1)
+        )
+        return amp * w * np.exp(lnprefactor + lnbody)
+
+    def GetPositionAreaHeightFWHMFromSinglePeakParameters(
+        self, parameters, indexOfPeak, cv=None
+    ):
+        amp = parameters[f"a{indexOfPeak}"]
+        loc = parameters[f"pos{indexOfPeak}"]
+        wm = parameters[f"w{indexOfPeak}"]
+        m = parameters[f"m{indexOfPeak}"]
+        vm = parameters[f"v{indexOfPeak}"]
+
+        area = PearsonIVAmplitudeParametrizationHPW.GetArea(amp, wm, m, vm)
+        pos = loc
+        height = amp
+        fwhm = PearsonIVAmplitudeParametrizationHPW.GetHWHM(
+            wm, m, vm, True
+        ) + PearsonIVAmplitudeParametrizationHPW.GetHWHM(wm, m, vm, False)
+
+        posStdDev = 0
+        areaStdDev = 0
+        heightStdDev = 0
+        fwhmStdDev = 0
+
+        if cv is not None:
+            deriv = np.zeros(5)
+            resVec = np.zeros(5)
+
+            # PositionVariance
+            posStdDev = SafeSqrt(cv[1, 1])
+
+            # Height variance
+            heightStdDev = SafeSqrt(cv[0, 0])
+
+            # Area variance
+            deriv[0] = PearsonIVAmplitudeParametrizationHPW.GetArea(1, wm, m, vm)
+            deriv[1] = 0
+            absDelta = wm * 1e-5
+            deriv[2] = (
+                PearsonIVAmplitudeParametrizationHPW.GetArea(amp, wm + absDelta, m, vm)
+                - PearsonIVAmplitudeParametrizationHPW.GetArea(
+                    amp, wm - absDelta, m, vm
+                )
+            ) / (2 * absDelta)
+            absDelta = m * 1e-5
+            deriv[3] = (
+                PearsonIVAmplitudeParametrizationHPW.GetArea(amp, wm, m + absDelta, vm)
+                - PearsonIVAmplitudeParametrizationHPW.GetArea(
+                    amp, wm, m - absDelta, vm
+                )
+            ) / (2 * absDelta)
+            absDelta = 1e-5 if vm == 0 else np.abs(vm * 1e-5)
+            deriv[4] = (
+                PearsonIVAmplitudeParametrizationHPW.GetArea(amp, wm, m, vm + absDelta)
+                - PearsonIVAmplitudeParametrizationHPW.GetArea(
+                    amp, wm, m, vm - absDelta
+                )
+            ) / (2 * absDelta)
+            resVec = cv.dot(deriv)
+            areaStdDev = SafeSqrt(np.dot(deriv, resVec))
+
+            # FWHM variance
+            deriv[0] = 0
+            deriv[1] = 0
+            absDelta = wm * 1e-5
+            deriv[2] = (
+                PearsonIVAmplitudeParametrizationHPW.GetFWHM(wm + absDelta, m, vm)
+                - PearsonIVAmplitudeParametrizationHPW.GetFWHM(wm - absDelta, m, vm)
+            ) / (2 * absDelta)
+            absDelta = m * 1e-5
+            deriv[3] = (
+                PearsonIVAmplitudeParametrizationHPW.GetFWHM(wm, m + absDelta, vm)
+                - PearsonIVAmplitudeParametrizationHPW.GetFWHM(wm, m - absDelta, vm)
+            ) / (2 * absDelta)
+            absDelta = 1e-5 if vm == 0 else np.abs(vm * 1e-5)
+            deriv[4] = (
+                PearsonIVAmplitudeParametrizationHPW.GetFWHM(wm, m, vm + absDelta)
+                - PearsonIVAmplitudeParametrizationHPW.GetFWHM(wm, m, vm - absDelta)
+            ) / (2 * absDelta)
+            resVec = cv.dot(deriv)
+            fwhmStdDev = SafeSqrt(np.dot(deriv, resVec))
+
+        return PositionAreaHeightFWHM(
+            pos, posStdDev, area, areaStdDev, height, heightStdDev, fwhm, fwhmStdDev
+        )
---
+++ tests/fitting_functions/test_pearsonivamplitudeparametrizationhpw.py
@@ -0,0 +1,350 @@
+from ramanchada2.fitting_functions import pearsonivamplitudeparametrizationhpw
+from ramanchada2.fitting_functions.pearsonivamplitudeparametrizationhpw import (
+    PearsonIVAmplitudeParametrizationHPW,
+)
+import lmfit
+import numpy as np
+
+
+def almostequal(desired, actual, abstol, reltol):
+    delta = np.abs(abstol) + np.abs(reltol * desired)
+    return np.abs(desired - actual) <= delta
+
+
+def test_FunctionValuesBasics():
+    # amp, pos, w, m, v
+    testData = [
+        (3, 7, 5, 11, 13),
+        (3, 7, 1 / 5.0, 1 / 1000.0, 1000),
+        (3, 7, 1 / 5.0, 1 / 1000.0, -1000),
+        (3, 7, 1 / 5.0, 1 / 1000.0, 0),
+        (3, 7, 1 / 5.0, 1000.0, 1000),
+        (3, 7, 1 / 5.0, 1000.0, -1000),
+        (3, 7, 1 / 5.0, 1000.0, 0),
+    ]
+
+    func = PearsonIVAmplitudeParametrizationHPW(1, -1)
+    for (amp, pos, w, m, v) in testData:
+        p = lmfit.Parameters()
+        p.add("a0", amp)
+        p.add("pos0", pos)
+        p.add("w0", w)
+        p.add("m0", m)
+        p.add("v0", v)
+
+        # Y at x=pos should be equal to amp
+        X = np.array([pos])
+        y = PearsonIVAmplitudeParametrizationHPW.GetYOfOneTerm(X[0], amp, pos, w, m, v)
+        assert almostequal(amp, y, 1e-10, 1e-10)
+        Y = func.func(p, X)
+        assert almostequal(y, Y[0], 1e-15, 1e-15)
+
+        # Y at x= pos - w /1000 should be less than amp
+        X = np.array([pos - w / 1000.0])
+        y = PearsonIVAmplitudeParametrizationHPW.GetYOfOneTerm(X[0], amp, pos, w, m, v)
+        assert amp > y
+        Y = func.func(p, X)
+        assert almostequal(y, Y[0], 1e-15, 1e-15)
+
+        # Y at x= pos + w /1000 should be less than amp
+        X = np.array([pos + w / 1000.0])
+        y = PearsonIVAmplitudeParametrizationHPW.GetYOfOneTerm(X[0], amp, pos, w, m, v)
+        assert amp > y
+        Y = func.func(p, X)
+        assert almostequal(y, Y[0], 1e-15, 1e-15)
+
+        # Y at x= pos -3 w should be less than amp/2
+        X = np.array([pos - 3 * w])
+        y = PearsonIVAmplitudeParametrizationHPW.GetYOfOneTerm(X[0], amp, pos, w, m, v)
+        assert amp / 2 > y
+        Y = func.func(p, X)
+        assert almostequal(y, Y[0], 1e-15, 1e-15)
+
+        # Y at x= pos + 3 w should be less than amp/2
+        X = np.array([pos + 3 * w])
+        y = PearsonIVAmplitudeParametrizationHPW.GetYOfOneTerm(X[0], amp, pos, w, m, v)
+        assert amp / 2 > y
+        Y = func.func(p, X)
+        assert almostequal(y, Y[0], 1e-15, 1e-15)
+
+
+def test_FunctionValuesInDependenceOnX():
+    testData = [
+        (-1e9, 3, 7, 5, 11, 13, 2.7867315365921926531e-160),
+        (-19, 3, 7, 5, 11, 13, 0.0071554075200805234436),
+        (-1, 3, 7, 5, 11, 13, 0.94609302487197904495),
+        (0, 3, 7, 5, 11, 13, 1.1845028216303614895),
+        (6, 3, 7, 5, 11, 13, 2.9207184451185306856),
+        (7, 3, 7, 5, 11, 13, 3.0000000000000000000),
+        (8, 3, 7, 5, 11, 13, 2.9093699544835131623),
+        (23, 3, 7, 5, 11, 13, 5.7924047779440271306e-25),
+        (28, 3, 7, 5, 11, 13, 7.9007462868389145363e-260),
+    ]
+
+    func = PearsonIVAmplitudeParametrizationHPW(1, -1)
+    for (x, amp, pos, w, m, v, yexpected) in testData:
+        p = lmfit.Parameters()
+        p.add("a0", amp)
+        p.add("pos0", pos)
+        p.add("w0", w)
+        p.add("m0", m)
+        p.add("v0", v)
+
+        X = np.array([x])
+        y = PearsonIVAmplitudeParametrizationHPW.GetYOfOneTerm(X[0], amp, pos, w, m, v)
+        if yexpected < 1e-100:
+            assert almostequal(yexpected, y, 0, 1e-7)
+        else:
+            assert almostequal(yexpected, y, 0, 1e-13)
+
+        Y = func.func(p, X)
+        assert almostequal(y, Y[0], 1e-15, 1e-15)
+
+
+def test_DerivativesWrtParameters():
+    ff = PearsonIVAmplitudeParametrizationHPW(1, -1)
+
+    # General case
+    amp = 17
+    pos = 7
+    w = 3
+    m = 5
+    v = 7
+
+    expectedFunctionValue = 10.456864176583425249219583024250
+    expectedDerivativeWrtAmplitude = 0.61510965744608383818938723672062
+    expectedDerivativeWrtPosition = 6.2023944308469781499266683603126
+    expectedDerivativeWrtW = 4.1349296205646520999511122402084
+    expectedDerivativeWrtM = 0.31213582841409496793210643128803
+    expectedDerivativeWrtV = -0.0080620681798224796387712054749779
+
+    p = lmfit.Parameters()
+    p.add("a0", amp)
+    p.add("pos0", pos)
+    p.add("w0", w)
+    p.add("m0", m)
+    p.add("v0", v)
+    X = np.array([9])
+    FV = ff.func(p, X)
+    DY = ff.dfunc(p, X)
+
+    assert almostequal(expectedFunctionValue, FV[0], 0, 1e-12)
+    assert almostequal(expectedDerivativeWrtAmplitude, DY[0, 0], 0, 1e-12)
+    assert almostequal(expectedDerivativeWrtPosition, DY[1, 0], 0, 1e-12)
+    assert almostequal(expectedDerivativeWrtW, DY[2, 0], 0, 1e-12)
+    assert almostequal(expectedDerivativeWrtM, DY[3, 0], 0, 1e-12)
+    assert almostequal(expectedDerivativeWrtV, DY[4, 0], 0, 1e-11)
+
+
+def test_AreaFwhm():
+    testData = [
+        (1, 7, 5, 11, 13),
+        (3, 7, 5, 1000, -1000),
+        (3, 7, 5, 1000, 0),
+        (3, 7, 5, 1000, 1000),
+        (3, 7, 5, 513 / 1024.0, -1000),
+        (3, 7, 5, 513 / 1024.0, 0),
+        (3, 7, 5, 513 / 1024.0, 1000),
+    ]
+
+    expectedAreas = [
+        11.017024491957193080,
+        31.945780266160134464,
+        31.940456418616702875,
+        31.945780266160134464,
+        12084.703859367018981,
+        8896.1196272036681069,
+        12084.703859367018981,
+    ]
+
+    expectedFwhm = [
+        10.069306246733044125,
+        10.000770127169960848,
+        10.000000000000000000,
+        10.000770127169960848,
+        11.364892411874591570,
+        10.000000000000000000,
+        11.364892411874591570,
+    ]
+
+    func = PearsonIVAmplitudeParametrizationHPW(1, -1)
+
+    for i in range(len(testData)):
+        (amp, pos, w, m, v) = testData[i]
+        p = lmfit.Parameters()
+        p.add("a0", amp)
+        p.add("pos0", pos)
+        p.add("w0", w)
+        p.add("m0", m)
+        p.add("v0", v)
+        (
+            position,
+            posErr,
+            area,
+            areaErr,
+            height,
+            heightErr,
+            fwhm,
+            fwhmErr,
+        ) = func.GetPositionAreaHeightFWHMFromSinglePeakParameters(p, 0)
+
+        assert almostequal(pos, position, 0, 1e-14)
+        assert almostequal(amp, height, 0, 1e-14)
+        assert almostequal(expectedAreas[i], area, 0, 1e-8)
+        assert almostequal(expectedFwhm[i], fwhm, 0, 1e-8)
+
+    # with the first test set, we test additionally
+    (amp, pos, w, m, v) = testData[0]
+    p = lmfit.Parameters()
+    p.add("a0", amp)
+    p.add("pos0", pos)
+    p.add("w0", w)
+    p.add("m0", m)
+    p.add("v0", v)
+    (
+        position,
+        posErr,
+        area,
+        areaErr,
+        height,
+        heightErr,
+        fwhm,
+        fwhmErr,
+    ) = func.GetPositionAreaHeightFWHMFromSinglePeakParameters(p, 0)
+
+    # (i) when we vary the amplitude, area and height should change accordingly, pos and fwhm stay constant
+    for newAmp in [-11, 0, 2, 17, 33, 1023, -11]:
+        p = lmfit.Parameters()
+        p.add("a0", newAmp)
+        p.add("pos0", pos)
+        p.add("w0", w)
+        p.add("m0", m)
+        p.add("v0", v)
+        (
+            newposition,
+            _,
+            newarea,
+            _,
+            newheight,
+            _,
+            newfwhm,
+            _,
+        ) = func.GetPositionAreaHeightFWHMFromSinglePeakParameters(p, 0)
+        assert almostequal(position, newposition, 0, 1e-14)
+        assert almostequal(height * newAmp, newheight, 0, 1e-14)
+        assert almostequal(area * newAmp, newarea, 0, 1e-14)
+        assert almostequal(fwhm, newfwhm, 0, 1e-14)
+
+    # (ii) when we vary the position, position should change accordingly, everything else is the same
+    for newPos in [-11, 0, 2, 17, 33, 1023, -11]:
+        p = lmfit.Parameters()
+        p.add("a0", amp)
+        p.add("pos0", newPos)
+        p.add("w0", w)
+        p.add("m0", m)
+        p.add("v0", v)
+        (
+            newposition,
+            _,
+            newarea,
+            _,
+            newheight,
+            _,
+            newfwhm,
+            _,
+        ) = func.GetPositionAreaHeightFWHMFromSinglePeakParameters(p, 0)
+        assert almostequal(newPos, newposition, 0, 1e-14)
+        assert almostequal(height, newheight, 0, 1e-14)
+        assert almostequal(area, newarea, 0, 1e-14)
+        assert almostequal(fwhm, newfwhm, 0, 1e-14)
+
+    # (iii) when we vary the width, area and fwhm should change accordingly, everything else is the same
+    for newW in [1 / 1000.0, 2, 17, 33, 1023]:
+        p = lmfit.Parameters()
+        p.add("a0", amp)
+        p.add("pos0", pos)
+        p.add("w0", newW)
+        p.add("m0", m)
+        p.add("v0", v)
+        (
+            newposition,
+            _,
+            newarea,
+            _,
+            newheight,
+            _,
+            newfwhm,
+            _,
+        ) = func.GetPositionAreaHeightFWHMFromSinglePeakParameters(p, 0)
+        assert almostequal(position, newposition, 0, 1e-14)
+        assert almostequal(height, newheight, 0, 1e-14)
+        assert almostequal(area * (newW / w), newarea, 0, 1e-14)
+        assert almostequal(fwhm * (newW / w), newfwhm, 0, 1e-14)
+
+
+def test_FwhmApproximation():
+    claimedMaxApproximationError = 0.19  # 19% error
+    w = 7
+    for idx_v in range(-1, 151):
+        v = 0 if idx_v < 0 else np.power(10, (idx_v - 75) / 25.0)
+        for idx_m in range(0, 151):
+            m = np.power(10, (idx_m - 75) / 25.0)
+            fwhmP = PearsonIVAmplitudeParametrizationHPW.GetFWHM(w, m, v)
+            fwhmN = PearsonIVAmplitudeParametrizationHPW.GetFWHM(w, m, -v)
+
+            fwhmApproxP = PearsonIVAmplitudeParametrizationHPW.GetFWHMApproximation(
+                w, m, v
+            )
+            fwhmApproxN = PearsonIVAmplitudeParametrizationHPW.GetFWHMApproximation(
+                w, m, -v
+            )
+
+            # FWHM should be independent of whether v is positive or negative
+            assert almostequal(fwhmP, fwhmN, 1e-8, 1e-8)
+
+            assert almostequal(fwhmP, fwhmApproxP, 0, claimedMaxApproximationError)
+            assert almostequal(fwhmN, fwhmApproxN, 0, claimedMaxApproximationError)
+
+
+def test_DerivativesOfAreaFwhm():
+    # see PearsonIV (amplitudeModified).nb
+    amp = 3
+    pos = 7
+    w = 5
+    m = 11
+    v = 13
+
+    ymaxDerivs = [1, 0, 0, 0, 0]
+    xmaxDerivs = [0, 1, 0, 0, 0]
+    areaDerivs = [
+        11.0170244919571930798,
+        0,
+        6.6102146951743158479,
+        -0.106359067713399883296,
+        0.00045052750734765666701,
+    ]
+    fwhmDerivs = [
+        0,
+        0,
+        2.0138612493466088250930420306,
+        -0.0062732720000624223737773164,
+        0.0000628521667333096080982645,
+    ]
+
+    p = lmfit.Parameters()
+    p.add("a0", amp)
+    p.add("pos0", pos)
+    p.add("w0", w)
+    p.add("m0", m)
+    p.add("v0", v)
+
+    func = PearsonIVAmplitudeParametrizationHPW(1, -1)
+
+    for i in range(0, 5):
+        cov = np.zeros((5, 5))
+        cov[i, i] = 1
+        result = func.GetPositionAreaHeightFWHMFromSinglePeakParameters(p, 0, cov)
+        assert almostequal(np.abs(ymaxDerivs[i]), result.HeightStdDev, 1e-13, 1e-7)
+        assert almostequal(np.abs(xmaxDerivs[i]), result.PositionStdDev, 1e-13, 1e-7)
+        assert almostequal(np.abs(areaDerivs[i]), result.AreaStdDev, 1e-13, 1e-4)
+        assert almostequal(np.abs(fwhmDerivs[i]), result.FWHMStdDev, 1e-13, 1e-4)
