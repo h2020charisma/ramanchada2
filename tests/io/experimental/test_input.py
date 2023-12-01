646474be9cc0bd5ea9bf288dda699ad88ff5eacc
vedina <jeliazkova.nina@gmail.com>
2023-12-01 19:46:41 +0200

    asserts

--- tests/io/experimental/test_input.py
+++ tests/io/experimental/test_input.py
@@ -3,6 +3,7 @@ from ramanchada2.io.experimental.rc1_parser.third_party_readers import readOPUS
 
 def test_opus(opus_experimental_file):
     x, y, meta = readOPUS(opus_experimental_file)
-    print(x)
-    print(meta)
-    #assert df.shape == (5, 10), 'wrong shape'
+    assert 4096 ==len(x)
+    assert 4096 ==len(y)
+    assert 94 ==len(meta)
+    assert meta["NPT"] == 4096
