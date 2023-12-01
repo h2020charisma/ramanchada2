f18a47b91233ec23aada460d70533abc441f7ddf
Luchesar ILIEV <luchesar.iliev@gmail.com>
2023-12-01 22:32:44 +0200

    tests: fix an assert

--- tests/io/experimental/test_input.py
+++ tests/io/experimental/test_input.py
@@ -5,7 +5,7 @@ def test_opus(opus_experimental_file):
     x, y, meta = readOPUS(opus_experimental_file)
     assert 4096 == len(x)
     assert 4096 == len(y)
-    assert 94 == len(meta)
+    assert 96 == len(meta)
     if "NPT" in meta:
         assert meta["NPT"] == 4096
     elif "AB Data Parameter.NPT" in meta:
