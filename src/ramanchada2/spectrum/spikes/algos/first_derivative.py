355aa0153cedc99f913accc840437a88013e5abb
Georgi Georgiev <georgiev.georgi.st@gmail.com>
2024-01-26 16:54:07 +0200

    remove misleading leftovers from first_derviative

--- src/ramanchada2/spectrum/spikes/algos/first_derivative.py
+++ src/ramanchada2/spectrum/spikes/algos/first_derivative.py
@@ -1,6 +1,9 @@
 import numpy as np
-def first_derivative(intensity, threshold):
-    # Using second order central differences for Laplacian
-    # The Laplacian of a 1D function is its second derivative
-    laplacian = np.diff(intensity, prepend = [0])    
-    return laplacian
+
+
+def metric(intensity):
+    return np.diff(intensity, prepend=[0])
+
+
+def indices(s, threshold=None):
+    ...
