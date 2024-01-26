a6e4f8fd9bb2b75d9ba53db51490aa9824e24ac6
Georgi Georgiev <georgiev.georgi.st@gmail.com>
2024-01-26 18:08:12 +0200

    flakify laplacian and mod_z_scores

--- src/ramanchada2/spectrum/spikes/algos/laplacian.py
+++ src/ramanchada2/spectrum/spikes/algos/laplacian.py
@@ -1,30 +1,36 @@
 import numpy as np
-def laplacian(intensity, threshold = 200):
+
+
+def laplacian(intensity, threshold=200):
     """
     This function returns the indices of posible spikes in a spectrum.
-    The function calculates the second derivative (laplacian) in one dimension of a spectrum and identifies the 
+    The function calculates the second derivative (laplacian) in one dimension of a spectrum and identifies the
     indices where te absolute value of this second derivative exceeds a specified threshold.
 
     Parameters:
     intensity (array): An array of intensity values (spectrum).
-    threshold (float, optional): A threshold value to identify significant changes in the intensity denoting a spike. Defaults to ???.
+    threshold (float, optional): A threshold value to identify significant changes in the intensity denoting a spike.
+    Defaults to ???.
 
     Returns:
-    array: An array of indices where posible spikes are present (or where the second derivative of the intensity exceeds the threshold).
+    array: An array of indices where posible spikes are present (or where the second derivative of the intensity
+    exceeds the threshold).
     """
 
-    ################# TO BE DONE #################
-    # Include calculation of automatic threshold #
-    #############################################
+    # ################ TO BE DONE #################
+    #  Include calculation of automatic threshold #
+    # ############################################
 
-    #y_merit = np.abs(np.diff(intensity, prepend=[0], append = [0])) # I did not used this because it results on a spike on the first and last indeces.    
+    # I did not used this because it results on a spike on the first and last indeces.
+    # y_merit = np.abs(np.diff(intensity, prepend=[0], append = [0]))
+
     y_merit = np.diff(intensity, n=2)
     y_merit = np.pad(y_merit, (1, 1), 'constant')
 
     # Compare the y_merit (absolute value of the second derivative or laplacian) with the threshold.
-    # This creates a boolean array where True (1) indicates a point where the absolute value of the derivative exceeds the threshold (spike).
+    # This creates a boolean array where True (1) indicates a point
+    # where the absolute value of the derivative exceeds the threshold (spike).
     spikes_yesno = y_merit > threshold
-  
 
     # Extract the indices where spikes_yesno is True (i.e., where the merit function exceeds the threshold).
     # np.where returns a tuple, and [0] accesses the first element of the tuple, which contains the indices.
