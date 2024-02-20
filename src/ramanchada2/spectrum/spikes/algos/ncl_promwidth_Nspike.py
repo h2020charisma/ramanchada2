2d86e88b9f380d3cb496a0f7acc99b6a6b0caf46
Nicolas Coca Lopez <nicolascocalopez@googlemail.com>
2024-02-20 13:11:43 +0100

    updated metric for ncl_prom_width

--- src/ramanchada2/spectrum/spikes/algos/ncl_promwidth_Nspike.py
+++ src/ramanchada2/spectrum/spikes/algos/ncl_promwidth_Nspike.py
@@ -3,13 +3,24 @@ from scipy.signal import find_peaks, peak_widths
 from scipy.stats import median_abs_deviation
 
 
-def metric(y):
-    metr = np.zeros_like(y)
-    metr[indices(y)] = 1
-    return metr
+def metric(y, prominence_threshold=None):
+    """
+    https://doi.org/10.1016/j.aca.2024.342312
+    """
+    if prominence_threshold is None:
+        prominence_threshold = prominence_threshold = 5*median_abs_deviation((np.diff(y, prepend=[0])))
+    peaks, _ = find_peaks(y, prominence=prominence_threshold)
+    widths = peak_widths(y, peaks, rel_height=0.5)[0]
+    y_merit = np.zeros_like(y)
+    for peak, width in zip(peaks, widths):
+        y_merit[peak] = width
+    return y_merit
 
 
 def indices(y, width_threshold=None, prominence_threshold=None, width_param_rel=None):
+    """
+    https://doi.org/10.1016/j.aca.2024.342312
+    """
     if width_threshold is None:
         width_threshold = 3
     if prominence_threshold is None:
