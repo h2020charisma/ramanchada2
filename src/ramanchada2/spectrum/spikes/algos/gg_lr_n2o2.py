4ca85d2e48518ffd323a1573b2761b24c8b6c800
Georgi Georgiev <georgiev.georgi.st@gmail.com>
2024-03-11 13:25:13 +0200

    Optimize calculations using numpy arrays

--- src/ramanchada2/spectrum/spikes/algos/gg_lr_n2o2.py
+++ src/ramanchada2/spectrum/spikes/algos/gg_lr_n2o2.py
@@ -5,8 +5,10 @@ from pydantic import validate_arguments
 
 
 def metric(y):
-    l2o2 = [3*y[i-2]-2*y[i-3] for i in range(3, len(y)-3)]
-    r2o2 = [3*y[i+2]-2*y[i+3] for i in range(3, len(y)-3)]
+    #l2o2 = [3*y[i-2]-2*y[i-3] for i in range(3, len(y)-3)]
+    #r2o2 = [3*y[i+2]-2*y[i+3] for i in range(3, len(y)-3)]
+    l2o2 = 3*y[1: -5] - 2*y[0:-6]
+    r2o2 = 3*y[5: -1] - 2*y[6:]
     y = y[3:-3]
     metric = np.max([y-np.max([l2o2, r2o2], axis=0), np.min([l2o2, r2o2], axis=0)-y], axis=0)
     return np.pad(metric, (3, 3))
