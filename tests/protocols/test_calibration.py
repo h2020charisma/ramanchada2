35b8f1e4b972373deacb769f7e36ddbd539a6dbe
vedina <jeliazkova.nina@gmail.com>
2024-06-04 15:18:39 +0300

    added test file

--- tests/protocols/test_calibration.py
+++ tests/protocols/test_calibration.py
@@ -3,13 +3,9 @@
 import numpy as np
 import pytest
 import ramanchada2 as rc2
-from ramanchada2.protocols.calibration import CalibrationModel, XCalibrationComponent
-
+from ramanchada2.protocols import CalibraitonModel 
 from sklearn.metrics.pairwise import cosine_similarity
 import ramanchada2.misc.constants as rc2const
-import matplotlib.pyplot as plt
-
-from ramanchada2.protocols.serialization import save_xcalibration_model
 
 NEON_WL = {
     785: rc2const.neon_wl_785_nist_dict,
@@ -17,33 +13,8 @@ NEON_WL = {
     532: rc2const.neon_wl_532_nist_dict
 }
 
-class SetupModule:
-    def __init__(self):
-        self.spe_neon = rc2.spectrum.from_test_spe(sample=['Neon'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
-        self.spe_pst2 = rc2.spectrum.from_test_spe(sample=['PST'], provider=['FNMT'], OP=['02'], laser_wl=['785'])
-        self.spe_pst3 = rc2.spectrum.from_test_spe(sample=['PST'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
-        self.spe_sil = rc2.spectrum.from_test_spe(sample=['S0B'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
-        self.spe_nCal = rc2.spectrum.from_test_spe(sample=['nCAL'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
-
-        self.spe_sil = self.spe_sil.trim_axes(method='x-axis',boundaries=(520.45-200,520.45+200))
-        self.spe_neon = self.spe_neon.trim_axes(method='x-axis',boundaries=(100,max(self.spe_neon.x)))    
-        kwargs = {"niter" : 40 }
-        self.spe_neon = self.spe_neon.subtract_baseline_rc1_snip(**kwargs)  
-        self.spe_sil = self.spe_sil.subtract_baseline_rc1_snip(**kwargs)    
-
-        ## normalize min/max
-        self.spe_neon = self.spe_neon.normalize()        
-        self.spe_sil = self.spe_sil.normalize()        
-
-        self.calmodel, model_neon = calibration_model_x(785,self.spe_neon,self.spe_sil)
-        
-
-@pytest.fixture(scope='module')
-def setup_module():
-    return SetupModule()
-
 def calibration_model_x(laser_wl,spe_neon,spe_sil,neon_wl = NEON_WL):
-    calmodel = CalibrationModel(laser_wl)
+    calmodel = rc2.protocols.CalibrationModel(laser_wl)
     calmodel.prominence_coeff = 3
     model_neon = calmodel.derive_model_curve(spe_neon,neon_wl[laser_wl],spe_units="cm-1",ref_units="nm",find_kw={},fit_peaks_kw={},should_fit = False,name="Neon calibration")
     return calmodel, model_neon
@@ -58,37 +29,37 @@ def resample(spe,xmin,xmax,npoints):
     return y_values *  scale
 
 
-def test_xcalibration(setup_module):
+def test_xcalibration():
+    spe_neon = rc2.spectrum.from_test_spe(sample=['Neon'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
+    spe_pst2 = rc2.spectrum.from_test_spe(sample=['PST'], provider=['FNMT'], OP=['02'], laser_wl=['785'])
+    spe_pst3 = rc2.spectrum.from_test_spe(sample=['PST'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
+    spe_sil = rc2.spectrum.from_test_spe(sample=['S0B'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
+    spe_nCal = rc2.spectrum.from_test_spe(sample=['nCAL'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
+
+    spe_sil = spe_sil.trim_axes(method='x-axis',boundaries=(520.45-200,520.45+200))
+    spe_neon = spe_neon.trim_axes(method='x-axis',boundaries=(100,max(spe_neon.x)))    
+    kwargs = {"niter" : 40 }
+    spe_neon = spe_neon.subtract_baseline_rc1_snip(**kwargs)  
+    spe_sil = spe_sil.subtract_baseline_rc1_snip(**kwargs)    
+
+    ## normalize min/max
+    spe_neon = spe_neon.normalize()        
+    spe_sil = spe_sil.normalize()        
+
     spe_y_original = []
-    _min = 200
-    _max = 2000
-    spe_calibrated = []
-    fig, ax = plt.subplots(figsize=(24, 8))
-    for spe in [setup_module.spe_pst2,setup_module.spe_pst3]:
-        spe_norm = spe.normalize()
-        spe_norm.plot(ax=ax,label="original",color="blue")
-        spe_y_original.append(resample(spe_norm,_min,_max,_max-_min))
-        
-        spe = setup_module.calmodel.apply_calibration_x(
+    for spe in [spe_pst2,spe_pst3]:
+        spe_y_original.append(resample(spe,100,3100,3000))
+
+    calmodel, model_neon = calibration_model_x(785,spe_neon,spe_sil)
+    spe_y = []
+    for spe in [spe_pst2,spe_pst3]:
+        spe = calmodel.apply_calibration_x(
                 spe,
                 spe_units="cm-1"
                 )
-        #returns spectra in nm!
-        spe = setup_module.calmodel.components[0].convert_units(spe, "nm", "cm-1")
-        spe_norm = spe.normalize()        
-        spe_norm.plot(ax=ax,label="calibrated",color="red")
-        spe_calibrated.append(resample(spe_norm,_min,_max,_max-_min))
+        spe_y.append(resample(spe,100,3100,3000))
     cos_sim_matrix_original =  cosine_similarity(spe_y_original)
-    cos_sim_matrix =  cosine_similarity(spe_calibrated)
-
-    plt.savefig("{}.png".format("calibration"))
+    cos_sim_matrix =  cosine_similarity(spe_y)
     print(np.mean(cos_sim_matrix_original),np.mean(cos_sim_matrix))
-    assert(np.mean(cos_sim_matrix_original) <= np.mean(cos_sim_matrix))
-    
-def test_serialization(setup_module):
-    xcal = setup_module.calmodel.components[0]
-    print(type(xcal))
-    xcal.to_json("xcal.json")
-    
-    
+    #assert calibrated similarity is higher
     
