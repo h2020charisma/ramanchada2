5a779dcf287ea62a35600e76b12cb73e3b15ec42
Georgi Georgiev <georgiev.georgi.st@gmail.com>
2024-07-26 18:07:59 +0300

    Improve text file input parsers
    - support for spc in the native backend
    - tests for multiple input file formats
    - add `@signal` and `@axes` metadata if determined from input
    
    closes #132

--- tests/spectrum/creators/test_from_local_file.py
+++ tests/spectrum/creators/test_from_local_file.py
@@ -3,10 +3,16 @@ import numpy as np
 import ramanchada2 as rc2
 
 
-def test_from_local_file_rruf(rruf_experimental_filename):
-    spe = rc2.spectrum.from_local_file(rruf_experimental_filename)
+def test_from_local_file(experimental_filename):
+    if experimental_filename[-4:] in {'.l6s', '.wxd'}:
+        return
+    elif experimental_filename[-4:] in {'.wdf'}:
+        spe = rc2.spectrum.from_local_file(experimental_filename)
+    else:
+        spe = rc2.spectrum.from_local_file(experimental_filename, backend='native')
     assert len(spe.meta.__root__) > 0
     assert np.all(np.isfinite(spe.x))
     assert np.all(np.isfinite(spe.y))
     assert len(spe.x) == len(spe.y)
     assert len(spe.x) > 0
+    assert np.all(np.diff(spe.x) > 0)
