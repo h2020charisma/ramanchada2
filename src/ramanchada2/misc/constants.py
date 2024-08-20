5beb8a71c916f157bd23f44f7675bfe22c5dc916
vedina <jeliazkova.nina@gmail.com>
2024-08-20 10:51:23 +0300

    dict for Neon peaks

--- src/ramanchada2/misc/constants.py
+++ src/ramanchada2/misc/constants.py
@@ -539,3 +539,10 @@ neon_wl_D3_3 = [
 ]
 
 neon_wl_D3_3_dict = dict(zip(neon_wl_D3_3, [1]*len(neon_wl_D3_3)))
+
+NEON_WL = {
+    785: neon_wl_785_nist_dict,
+    633: neon_wl_633_nist_dict,
+    532: neon_wl_532_nist_dict
+}
+
