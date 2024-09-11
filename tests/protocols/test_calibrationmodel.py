266d6a927e53cb66047cc27eebd13c461b077fd9
Luchesar ILIEV <luchesar.iliev@gmail.com>
2024-09-11 15:04:47 +0300

    lint: Black-ify

--- tests/protocols/test_calibrationmodel.py
+++ tests/protocols/test_calibrationmodel.py
@@ -4,7 +4,11 @@ import numpy as np
 import pytest
 import ramanchada2 as rc2
 from ramanchada2.protocols.calibration import CalibrationModel
-from ramanchada2.protocols.calibration import YCalibrationComponent, YCalibrationCertificate, CertificatesDict
+from ramanchada2.protocols.calibration import (
+    YCalibrationComponent,
+    YCalibrationCertificate,
+    CertificatesDict,
+)
 from sklearn.metrics.pairwise import cosine_similarity
 import ramanchada2.misc.constants as rc2const
 import matplotlib.pyplot as plt
@@ -13,21 +17,40 @@ import matplotlib.pyplot as plt
 class SetupModule:
     def __init__(self):
         self.laser_wl = 785
-        self.spe_neon = rc2.spectrum.from_test_spe(sample=['Neon'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
-        self.spe_pst2 = rc2.spectrum.from_test_spe(sample=['PST'], provider=['FNMT'], OP=['02'], laser_wl=['785'])
-        self.spe_pst3 = rc2.spectrum.from_test_spe(sample=['PST'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
-        self.spe_sil = rc2.spectrum.from_test_spe(sample=['S0B'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
-        self.spe_sil2 = rc2.spectrum.from_test_spe(sample=['S0B'], provider=['FNMT'], OP=['02'], laser_wl=['785'])
-        self.spe_nCal = rc2.spectrum.from_test_spe(sample=['nCAL'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
+        self.spe_neon = rc2.spectrum.from_test_spe(
+            sample=["Neon"], provider=["FNMT"], OP=["03"], laser_wl=["785"]
+        )
+        self.spe_pst2 = rc2.spectrum.from_test_spe(
+            sample=["PST"], provider=["FNMT"], OP=["02"], laser_wl=["785"]
+        )
+        self.spe_pst3 = rc2.spectrum.from_test_spe(
+            sample=["PST"], provider=["FNMT"], OP=["03"], laser_wl=["785"]
+        )
+        self.spe_sil = rc2.spectrum.from_test_spe(
+            sample=["S0B"], provider=["FNMT"], OP=["03"], laser_wl=["785"]
+        )
+        self.spe_sil2 = rc2.spectrum.from_test_spe(
+            sample=["S0B"], provider=["FNMT"], OP=["02"], laser_wl=["785"]
+        )
+        self.spe_nCal = rc2.spectrum.from_test_spe(
+            sample=["nCAL"], provider=["FNMT"], OP=["03"], laser_wl=["785"]
+        )
 
-        self.spe_SRM2241 = rc2.spectrum.from_test_spe(sample=['NIST785_SRM2241'],
-                                                        provider=['FNMT'], OP=['03'], laser_wl=['785'])
+        self.spe_SRM2241 = rc2.spectrum.from_test_spe(
+            sample=["NIST785_SRM2241"], provider=["FNMT"], OP=["03"], laser_wl=["785"]
+        )
         # NIR785_EL0-9002
 
-        self.spe_sil = self.spe_sil.trim_axes(method='x-axis', boundaries=(max(100, 520.45-200), 520.45+200))
-        self.spe_sil2 = self.spe_sil2.trim_axes(method='x-axis', boundaries=(max(100, 520.45-200), 520.45+200))
+        self.spe_sil = self.spe_sil.trim_axes(
+            method="x-axis", boundaries=(max(100, 520.45 - 200), 520.45 + 200)
+        )
+        self.spe_sil2 = self.spe_sil2.trim_axes(
+            method="x-axis", boundaries=(max(100, 520.45 - 200), 520.45 + 200)
+        )
 
-        self.spe_neon = self.spe_neon.trim_axes(method='x-axis', boundaries=(100, max(self.spe_neon.x)))
+        self.spe_neon = self.spe_neon.trim_axes(
+            method="x-axis", boundaries=(100, max(self.spe_neon.x))
+        )
         kwargs = {"niter": 40}
         self.spe_neon = self.spe_neon.subtract_baseline_rc1_snip(**kwargs)
         self.spe_sil = self.spe_sil.subtract_baseline_rc1_snip(**kwargs)
@@ -40,9 +63,15 @@ class SetupModule:
 
         try:
             neon_wl = rc2const.NEON_WL[785]
-            self.calmodel = CalibrationModel.calibration_model_factory(785,
-                                                self.spe_neon, self.spe_sil, neon_wl=neon_wl,
-                                                find_kw={"wlen": 200, "width":  1}, fit_peaks_kw={}, should_fit=False)
+            self.calmodel = CalibrationModel.calibration_model_factory(
+                785,
+                self.spe_neon,
+                self.spe_sil,
+                neon_wl=neon_wl,
+                find_kw={"wlen": 200, "width": 1},
+                fit_peaks_kw={},
+                should_fit=False,
+            )
             assert len(self.calmodel.components) == 2
             # print(self.calmodel.components[1].profile, self.calmodel.components[1].peaks)
         except Exception as _err:
@@ -50,7 +79,7 @@ class SetupModule:
             print(_err)
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def setup_module():
     return SetupModule()
 
@@ -59,9 +88,8 @@ def test_laser_zeroing(setup_module):
     assert setup_module.calmodel is not None
     fig, ax = plt.subplots(1, 1, figsize=(12, 2))
     spe_sil_calib = setup_module.calmodel.apply_calibration_x(
-                setup_module.spe_sil,
-                spe_units="cm-1"
-                )
+        setup_module.spe_sil, spe_units="cm-1"
+    )
     setup_module.spe_sil.plot(label="Si original", ax=ax)
     spe_sil_calib.plot(ax=ax, label="Si laser zeroed", fmt=":")
     # ax.set_xlim(520.45-50,520.45+50)
@@ -77,17 +105,19 @@ def resample(spe, xmin, xmax, npoints):
     scale = np.max(spe.y) / np.max(y_values)
     # pdf sampling is normalized to area unity, scaling back
     # tbd - make resample a filter
-    return y_values*scale
+    return y_values * scale
 
 
 def resample_spline(spe, xmin, xmax, npoints):
-    spe_resampled = spe.resample_spline_filter((xmin, xmax), xnew_bins=1000, spline='akima', cumulative=False)
+    spe_resampled = spe.resample_spline_filter(
+        (xmin, xmax), xnew_bins=1000, spline="akima", cumulative=False
+    )
     # spe_resampled = spe.resample_NUDFT_filter(x_range=(xmin,xmax), xnew_bins=npoints)
     return spe_resampled
 
 
 def compare_calibrated_spe(setup_module, spectra, name="calibration"):
-    fig, ax = plt.subplots(len(spectra)+1, 1, figsize=(24, 8))
+    fig, ax = plt.subplots(len(spectra) + 1, 1, figsize=(24, 8))
     setup_module.calmodel.plot(ax=ax[0])
     crl = [("blue", "red"), ("green", "gray")]
     spe_y_original = []
@@ -97,18 +127,17 @@ def compare_calibrated_spe(setup_module, spectra, name="calibration"):
     for index, spe in enumerate(spectra):
         spe_norm = spe.normalize(strategy="unity_area")
         # resample with spline
-        spe_norm = resample_spline(spe_norm, _min, _max, _max-_min)
+        spe_norm = resample_spline(spe_norm, _min, _max, _max - _min)
         # resample with histogram
         # spe_y_original.append(resample(spe_norm,_min,_max,_max-_min))
-        spe_norm.plot(ax=ax[index+1], label=f"original {index}", color=crl[index][0])
+        spe_norm.plot(ax=ax[index + 1], label=f"original {index}", color=crl[index][0])
         spe_y_original.append(spe_norm.y)
-        spe_c = setup_module.calmodel.apply_calibration_x(
-                spe,
-                spe_units="cm-1"
-                )
+        spe_c = setup_module.calmodel.apply_calibration_x(spe, spe_units="cm-1")
         spe_c_norm = spe_c.normalize(strategy="unity_area")
-        spe_c_norm = resample_spline(spe_c_norm, _min, _max, _max-_min)
-        spe_c_norm.plot(ax=ax[index+1], label=f"calibrated {index}", color=crl[index][1])
+        spe_c_norm = resample_spline(spe_c_norm, _min, _max, _max - _min)
+        spe_c_norm.plot(
+            ax=ax[index + 1], label=f"calibrated {index}", color=crl[index][1]
+        )
         spe_calibrated.append(spe_c_norm.y)
     cos_sim_matrix_original = cosine_similarity(spe_y_original)
     cos_sim_matrix = cosine_similarity(spe_calibrated)
@@ -120,17 +149,23 @@ def compare_calibrated_spe(setup_module, spectra, name="calibration"):
 
 def test_xcalibration_pst(setup_module):
     assert setup_module.calmodel is not None
-    compare_calibrated_spe(setup_module, [setup_module.spe_pst2, setup_module.spe_pst3], name="PST")
+    compare_calibrated_spe(
+        setup_module, [setup_module.spe_pst2, setup_module.spe_pst3], name="PST"
+    )
 
 
 def test_xcalibration_si(setup_module):
     assert setup_module.calmodel is not None
-    compare_calibrated_spe(setup_module, [setup_module.spe_sil, setup_module.spe_sil2], name="Sil")
+    compare_calibrated_spe(
+        setup_module, [setup_module.spe_sil, setup_module.spe_sil2], name="Sil"
+    )
 
 
 def test_xcalibration_cal(setup_module):
     assert setup_module.calmodel is not None
-    compare_calibrated_spe(setup_module, [setup_module.spe_nCal, setup_module.spe_nCal], name="nCal")
+    compare_calibrated_spe(
+        setup_module, [setup_module.spe_nCal, setup_module.spe_nCal], name="nCal"
+    )
 
 
 def test_ycalibration(setup_module):
@@ -140,18 +175,24 @@ def test_ycalibration(setup_module):
     key = "NIST785_SRM2241"
     certificate = certs[key]
     certificate.plot(ax=ax[2], color="pink")
-    ycal = YCalibrationComponent(setup_module.laser_wl, setup_module.spe_SRM2241, certificate=certificate)
+    ycal = YCalibrationComponent(
+        setup_module.laser_wl, setup_module.spe_SRM2241, certificate=certificate
+    )
 
-    spe_to_correct = rc2.spectrum.from_test_spe(sample=['PST'], provider=['FNMT'], OP=['03'], laser_wl=['785'])
+    spe_to_correct = rc2.spectrum.from_test_spe(
+        sample=["PST"], provider=["FNMT"], OP=["03"], laser_wl=["785"]
+    )
     # spe_to_correct = spe_to_correct.trim_axes(method='x-axis',boundaries=(100,2000))
     # remove the pedestal
-    spe_to_correct.y = spe_to_correct.y-min(spe_to_correct.y)
+    spe_to_correct.y = spe_to_correct.y - min(spe_to_correct.y)
     spe_to_correct.plot(ax=ax[0], label="PST")
 
     window_length = 5
     maxy = max(spe_to_correct.y)
-    spe_to_correct = spe_to_correct.smoothing_RC1(method="savgol", window_length=window_length, polyorder=3)
-    spe_to_correct.y = maxy*spe_to_correct.y/max(spe_to_correct.y)
+    spe_to_correct = spe_to_correct.smoothing_RC1(
+        method="savgol", window_length=window_length, polyorder=3
+    )
+    spe_to_correct.y = maxy * spe_to_correct.y / max(spe_to_correct.y)
     spe_to_correct.plot(ax=ax[0], label="smoothed")
     setup_module.spe_SRM2241.plot(ax=ax[0].twinx(), label=key)
     spe_ycalibrated = ycal.process(spe_to_correct)
@@ -168,8 +209,8 @@ def test_ycertificate():
         params="A0 = 9.71937e-02, A1 = 2.28325e-04, A2 = -5.86762e-08, A3 = 2.16023e-10, A4 = -9.77171e-14, A5 = 1.15596e-17",
         equation="A0 + A1 * x + A2 * x**2 + A3 * x**3 + A4 * x**4 + A5 * x**5",
         temperature_c=(20, 25),
-        raman_shift=(200, 3500)
-        )
+        raman_shift=(200, 3500),
+    )
     cert.plot()
     plt.savefig("test_calmodel_{}.png".format("ycert"))
 
