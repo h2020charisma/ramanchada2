ae9a98a05cde70f972f1688ee468a886a59166c4
Georgi Georgiev <georgiev.georgi.st@gmail.com>
2024-09-13 13:17:27 +0300

    add docstrings

--- src/ramanchada2/spectrum/calibration/change_x_units.py
+++ src/ramanchada2/spectrum/calibration/change_x_units.py
@@ -14,6 +14,15 @@ from ..spectrum import Spectrum
 @validate_call(config=dict(arbitrary_types_allowed=True))
 def abs_nm_to_shift_cm_1(spe: Spectrum, /,
                          laser_wave_length_nm: float):
+    """
+    Convert wavelength to Ramanshift in wavenumber
+
+    Args:
+        spe: internal use only
+        laser_wave_length_nm: Laser wave length
+
+    Returns: Corrected x-values
+    """
     return util_abs_nm_to_shift_cm_1(spe.x, laser_wave_length_nm=laser_wave_length_nm)
 
 
@@ -21,6 +30,15 @@ def abs_nm_to_shift_cm_1(spe: Spectrum, /,
 @validate_call(config=dict(arbitrary_types_allowed=True))
 def shift_cm_1_to_abs_nm(spe: Spectrum, /,
                          laser_wave_length_nm: float):
+    """
+    Convert Ramanshift in wavenumber to wavelength
+
+    Args:
+        spe: internal use only
+        laser_wave_length_nm: Laser wave length
+
+    Returns: Corrected x-values
+    """
     return util_shift_cm_1_to_abs_nm(spe.x, laser_wave_length_nm=laser_wave_length_nm)
 
 
@@ -29,6 +47,15 @@ def shift_cm_1_to_abs_nm(spe: Spectrum, /,
 def abs_nm_to_shift_cm_1_filter(old_spe: Spectrum,
                                 new_spe: Spectrum, /,
                                 laser_wave_length_nm: float):
+    """
+    Convert wavelength to Ramanshift in wavenumber
+
+    Args:
+        spe: internal use only
+        laser_wave_length_nm: Laser wave length
+
+    Returns: Spectrum with corrected x-values
+    """
     new_spe.x = util_abs_nm_to_shift_cm_1(old_spe.x, laser_wave_length_nm=laser_wave_length_nm)
 
 
@@ -37,4 +64,13 @@ def abs_nm_to_shift_cm_1_filter(old_spe: Spectrum,
 def shift_cm_1_to_abs_nm_filter(old_spe: Spectrum,
                                 new_spe: Spectrum, /,
                                 laser_wave_length_nm: float):
+    """
+    Convert Ramanshift in wavenumber to wavelength
+
+    Args:
+        spe: internal use only
+        laser_wave_length_nm: Laser wave length
+
+    Returns: Spectrum with corrected x-values
+    """
     new_spe.x = util_shift_cm_1_to_abs_nm(old_spe.x, laser_wave_length_nm=laser_wave_length_nm)
