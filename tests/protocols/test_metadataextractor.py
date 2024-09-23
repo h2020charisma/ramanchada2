3cbe89816abd6bd05edda5ff83c780e1bac0622f
vedina <jeliazkova.nina@gmail.com>
2024-09-24 00:06:55 +0300

    metadata extractor initial commit

--- tests/protocols/test_metadataextractor.py
+++ tests/protocols/test_metadataextractor.py
@@ -3,7 +3,8 @@ import pandas as pd
 from ramanchada2.auxiliary.spectra.datasets2 import (filtered_df,get_filenames,
                                                      prepend_prefix)
 import pytest
-
+from ramanchada2.protocols.metadata_helper import TemplateMetadataExtractor,SpectrumMetadataExtractor
+from ramanchada2.protocols.metadata_helper import FilenameMetadataExtractor,ChainedMetadataExtractor
 
 @pytest.fixture(scope="module")
 def spectra2test():
@@ -34,3 +35,19 @@ def test_spectraframe(spectra2test):
     assert("optical_path" in spe_frame.columns)
     assert("file_name" in spe_frame.columns)
     
+
+def test_tbd():
+
+    # Initialize individual metadata extractors
+    template_extractor = TemplateMetadataExtractor(template=['name'])
+    filename_extractor = FilenameMetadataExtractor()
+    spectrum_extractor = SpectrumMetadataExtractor()
+
+    # Chain the metadata extractors
+    chained_extractor = ChainedMetadataExtractor(template_extractor, filename_extractor, spectrum_extractor)
+
+    # Create the data table
+    data_table_creator = SpectraFrame.from_metadata(spectrum_extractor)
+    data_table = data_table_creator.create_datatable()
+
+    print(data_table)        
