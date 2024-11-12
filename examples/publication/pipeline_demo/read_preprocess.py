9f6d2cbb47cbf6c1c5adf86a73f77dd70e49aa63
vedina <jeliazkova.nina@gmail.com>
2024-11-12 13:56:35 +0200

    simple ploomber workflow

---
+++ examples/publication/pipeline_demo/read_preprocess.py
@@ -0,0 +1,23 @@
+# + tags=["parameters"]
+upstream = []
+product = None
+input_file = None
+# -
+
+import os.path
+import ramanchada2 as rc2
+
+
+file = os.path.abspath(input_file)
+
+# load spectrum from file system
+spe = rc2.spectrum.from_local_file(file)
+spe.plot()
+
+if os.path.isfile(product["cha"]):
+    os.remove(product["cha"])
+
+spe.write_cha(product["cha"], dataset="/raw")
+
+spe.subtract_baseline_rc1_snip(niter=40)
+spe.write_cha(product["cha"], dataset="/baseline")
